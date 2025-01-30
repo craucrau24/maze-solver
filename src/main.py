6ae17202eebcf6e844f39765ac59e872ac1bc20b
Crawcraw <crawcraw@crau.at.home>
2025-01-30 23:05:19 +0100

    implement draw_line() method

--- src/main.py
+++ src/main.py
@@ -1,7 +1,11 @@
 from window import Window
+from point import Point
+from line import Line
 
 def main():
     win = Window(800, 600)
+    win.draw_line(Line(Point(10, 50), Point(200,150)), "blue")
+    win.draw_line(Line(Point(410, 350), Point(100,250)), "red")
     win.wait_for_close()
 
 
