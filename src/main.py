b2b8cb39c5d7e9a1ea512fe8949f5ec0f781661f
Crawcraw <crawcraw@crau.at.home>
2025-01-31 14:59:09 +0100

    maze creation stub. drawing it (rows and cols are inverted at the moment)

--- src/main.py
+++ src/main.py
@@ -1,39 +1,10 @@
 from gui.window import Window
 from gui.point import Point
-from maze.cell import Cell
-
-def create_cells(data, win):
-    cells = []
-    for coords, walls in data:
-        x1, y1, x2, y2 = coords
-        cell = Cell(win, Point(x1, y1), Point(x2, y2))
-        if "left" not in walls:
-            cell.has_left_wall = False
-        if "right" not in walls:
-            cell.has_right_wall = False
-        if "top" not in walls:
-            cell.has_top_wall = False
-        if "bottom" not in walls:
-            cell.has_bottom_wall = False
-        cells.append(cell)
-    return cells
+from maze.maze import Maze
 
 def main():
     win = Window(800, 600)
-    cell_data = [
-        ((20, 20, 40, 40), set(["left", "right", "top", "bottom"])),
-        ((60, 40, 80, 60), set(["left", "right"])),
-        ((100, 60, 120, 80), set(["top", "bottom"])),
-        ((140, 80, 160, 100), set(["left", "top"])),
-        ((180, 100, 200, 120), set(["right", "bottom"])),
-
-    ]
-    cells = create_cells(cell_data, win)
-    for cell in cells:
-        cell.draw()
-    
-    cells[0].draw_move(cells[1], True)
-    cells[2].draw_move(cells[3])
+    maze = Maze(Point(20, 20), 15, 20, 30, 30, win)
     
     win.wait_for_close()
 
---
+++ src/maze/maze.py
@@ -0,0 +1,31 @@
+from .cell import Cell
+from gui.point import Point
+
+class Maze:
+    def __init__(self, origin, num_rows, num_cols, cell_size_x, cell_size_y, win):
+        self.origin = origin
+        self.num_rows = num_rows
+        self.num_cols = num_cols
+        self.cell_size_x = cell_size_x
+        self.cell_size_y = cell_size_y
+        self.__win = win
+
+        self._create_cells()
+
+    def _create_cells(self):
+        self.__cells = [
+            [self._create_cell(i, j) for j in range(self.num_cols)] for i in range(self.num_rows)
+        ]
+        
+        for row in self.__cells:
+            for cell in row:
+                cell.draw()
+
+    def _create_cell(self, i, j):
+        x = self.origin.x + i * self.cell_size_x
+        y = self.origin.y + j * self.cell_size_y
+        cell = Cell(self.__win, Point(x, y), Point(x + self.cell_size_x, y + self.cell_size_y))
+        return cell
+
+    def _animate(self):
+        pass
