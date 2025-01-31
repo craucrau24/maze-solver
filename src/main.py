1636c511bd0b13c09571621c796cc2c95a91e480
Crawcraw <christophe.raucroy@gmail.com>
2025-01-31 12:36:23 +0100

    add Cell class in maze subfolder

--- src/main.py
+++ src/main.py
@@ -1,11 +1,35 @@
 from gui.window import Window
 from gui.point import Point
-from gui.line import Line
+from maze.cell import Cell
+
+def create_cells(data, win):
+    cells = []
+    for coords, walls in data:
+        x1, y1, x2, y2 = coords
+        cell = Cell(win, Point(x1, y1), Point(x2, y2))
+        if "left" not in walls:
+            cell.has_left_wall = False
+        if "right" not in walls:
+            cell.has_right_wall = False
+        if "top" not in walls:
+            cell.has_top_wall = False
+        if "bottom" not in walls:
+            cell.has_bottom_wall = False
+        cells.append(cell)
+    return cells
 
 def main():
     win = Window(800, 600)
-    win.draw_line(Line(Point(10, 50), Point(200,150)), "blue")
-    win.draw_line(Line(Point(410, 350), Point(100,250)), "red")
+    cell_data = [
+        ((20, 20, 40, 40), set(["left", "right", "top", "bottom"])),
+        ((60, 40, 80, 60), set(["left", "right"])),
+        ((100, 60, 120, 80), set(["top", "bottom"])),
+        ((140, 80, 160, 100), set(["left", "top"])),
+        ((180, 100, 200, 120), set(["right", "bottom"])),
+
+    ]
+    for cell in create_cells(cell_data, win):
+        cell.draw()
     win.wait_for_close()
 
 
---
+++ src/maze/cell.py
@@ -0,0 +1,30 @@
+from gui.point import Point
+from gui.line import Line
+
+class Cell:
+    def __init__(self, win, topleft, botright):
+        self.has_left_wall = True
+        self.has_right_wall = True
+        self.has_top_wall = True
+        self.has_bottom_wall = True
+
+        self.__topleft = topleft
+        self.__botright = botright
+        self.__win = win
+    
+    def draw(self):
+        lines = []
+        topright = Point(self.__botright.x, self.__topleft.y)
+        botleft = Point(self.__topleft.x, self.__botright.y)
+
+        if self.has_left_wall:
+            lines.append(Line(botleft, self.__topleft))
+        if self.has_right_wall:
+            lines.append(Line(self.__botright, topright))
+        if self.has_top_wall:
+            lines.append(Line(self.__topleft, topright))
+        if self.has_bottom_wall:
+            lines.append(Line(self.__botright, botleft))
+        
+        for line in lines:
+            self.__win.draw_line(line, "black")
